8e7167e6e7443e43af01fd2fd6189df50d6fdd02
Carlos Robles <chrobles@pm.me>
2023-03-03 16:52:04 -0500

    Shape created and added to scence in main.js

--- portfolioThreeJS/main.js
+++ portfolioThreeJS/main.js
@@ -1,6 +1,6 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
@@ -8,10 +8,34 @@ const camera = new THREE.PerspectiveCamera(
   75, //FOV
   window.innerWidth / window.innerHeight, //Aspect Ratio
   0.1, // View Frustum Range min, max
-  1000 ); 
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#background'),
+  canvas: document.querySelector("#background"),
 });
 
-renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff6347,
+  wireframe: true,
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x;
+
+  renderer.render(scene, camera);
+}
+
+animate();
