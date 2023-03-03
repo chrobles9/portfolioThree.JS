4d89ae809ad573821178c4e7c1ba914a04d33d93
Carlos Robles <chrobles@pm.me>
2023-03-03 17:26:24 -0500

    Sphere shapes created with random generator to fill background around torus.

--- portfolioThreeJS/main.js
+++ portfolioThreeJS/main.js
@@ -59,4 +59,17 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+function createStar() {
+  const geometry = new THREE.SphereGeometry (0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+
+Array(200).fill().forEach(createStar);
 animate();
